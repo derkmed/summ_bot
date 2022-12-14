21aa8748e0065c393b13bc3d4121e3fc28340afc
Derek Ahmed <derekhmd@gmail.com>
2022-12-14 07:01:58 +0000

    Add CODS summaries

--- get_summaries.py
+++ get_summaries.py
@@ -1,29 +1,23 @@
 import random
-
-
-from datasets import load_dataset
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration, pipeline
-from transformers import BlenderbotConfig, BlenderbotForCausalLM
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import pipeline, pipelines
-
-
-
 import time
-import torch
+
 from typing import List
 
 from tqdm import tqdm
 
+from datasets import load_dataset, load_from_disk
+from datasets import Dataset
+from datasets.dataset_dict import DatasetDict
+
+from transformers import pipeline, pipelines
+
 def GetRandomTimesteps(dialog: List[str], t: int) -> List[int]:
-    return [random.randrange(0, len(dialog))]
+    # Ignore 0th timesteps.
+    return [random.randrange(1, len(dialog))]
 
 def GetTwoRandomTimesteps(dialog: List[str]) -> List[int]:
-    return [random.randrange(0, len(dialog)), random.randrange(0, len(dialog))]
-
-def PreProcessCods1(dialog: List[str]) -> str:
-    return "<s> <hl> {} <hl>".format(" <s> ".join(dialog))
+    # Ignore 0th timesteps.
+    return [random.randrange(1, len(dialog)), random.randrange(0, len(dialog))]
 
 def GetOneSentenceSummary(summarizer_pipeline: pipelines.text2text_generation.SummarizationPipeline, 
                 data: List[str]):
@@ -32,7 +26,7 @@ def GetOneSentenceSummary(summarizer_pipeline: pipelines.text2text_generation.Su
     '''
     batch_sketches = []
     batch_summaries = []
-    for i, out in enumerate(summarizer_pipeline(batch, batch_size=8)):
+    for i, out in enumerate(summarizer_pipeline(data, batch_size=8)):
         
         txt = out['summary_text']
         if "TLDR" not in txt:
@@ -59,6 +53,7 @@ def batched_random_start_augment(data):
     '''
     This augmentation is meant to be called with
     `.map(augment_data_batched, batched=True, remove_columns=...column_names, batch_size=...)`
+    It will explode all randomly-starting dialogs and output these with their respective random starts and total # timesteps.
     '''
     sliced = []
     timesteps = []
@@ -74,59 +69,209 @@ def batched_random_start_augment(data):
         "rand_start": sliced,
         }
 
-def summarizer_augment(summarizer, dataset):
-    TLDR_TOKEN = "TLDR" # " TLDR "?
-    sketches = []
-    summaries = []
-    raw = []
-    timesteps = []
-    for i, out in enumerate(summarizer(dataset['cods1'], batch_size=8)):
-        txt = out['summary_text']
-        if TLDR_TOKEN not in txt:
-            # We can't do anything so skip this sample.
-            continue
-        sketch, summary = txt.split(TLDR_TOKEN)
-        sketches.append(sketch)
-        summaries.append(summary)
-        raw.append(dataset['dialog'][i])
-        timesteps.append(dataset['timesteps'][i])
-    return raw, sketches, summaries, timesteps
+def GetCodsEnd(split_dataset):
+    '''
+    Cods1 augmentations are defined here without random start.
+    '''
 
-def summarizer_augment_dict(summarizer, dataset):
-    data, sketches, summaries, timesteps = summarizer_augment(summarizer, dataset)
-    return {
-        "data": data,
-        "sketch": sketches,
-        "summary": summaries,
-        "timesteps": timesteps
-    }
+    def _summarizer_augment(summarizer, dataset):
+        TLDR_TOKEN = "TLDR" # " TLDR "?
+        sketches = []
+        summaries = []
+        raw = []
+        for i, out in enumerate(summarizer(dataset['cods1'], batch_size=8)):
+            txt = out['summary_text']
+            if TLDR_TOKEN not in txt:
+                # We can't do anything so skip this sample.
+                continue
+            sketch, summary = txt.split(TLDR_TOKEN)
+            sketches.append(sketch)
+            summaries.append(summary)
+            raw.append(dataset['dialog'][i])
+        return raw, sketches, summaries
+
+    def _summarizer_augment_dict(summarizer, dataset):
+        data, sketches, summaries = _summarizer_augment(summarizer, dataset)
+        return {
+            "data": data,
+            "sketches": sketches,
+            "summary": summaries
+        }
+
+    def _preprocessCods1(dialog: List[str]) -> str:
+        return "<s> <hl> {} <hl>".format(" <s> ".join(dialog[:-1]))
+
+    cods1_preprocessed_data = split_dataset.map(lambda s: {"cods1": _preprocessCods1(s["dialog"])}, num_proc=4)
+    summarizer = pipeline("summarization", model="Salesforce/cods-bart-large-xsum-samsum", device=0)
+    aug_data = cods1_preprocessed_data.map(lambda s: _summarizer_augment_dict(summarizer, s),
+            batched=True, remove_columns=cods1_preprocessed_data.column_names)
+    return aug_data
+
+def GetCods1(split_dataset):
+    '''
+    Cods1 augmentations are defined here.
+    Important note: we want to 1:many map each dialog to multiple dialogs, *while* remove existing columns.
+    Doing so requires a lot of care with respect to columns and only works at the train/validation/test split level.
+    The final size of this dataset should be # random timesteps * # dialogs.
+    '''
+
+    def _summarizer_augment(summarizer, dataset):
+        TLDR_TOKEN = "TLDR" # " TLDR "?
+        sketches = []
+        summaries = []
+        raw = []
+        timesteps = []
+        for i, out in enumerate(summarizer(dataset['cods1'], batch_size=8)):
+            txt = out['summary_text']
+            if TLDR_TOKEN not in txt:
+                # We can't do anything so skip this sample.
+                continue
+            sketch, summary = txt.split(TLDR_TOKEN)
+            sketches.append(sketch)
+            summaries.append(summary)
+            raw.append(dataset['dialog'][i])
+            timesteps.append(dataset['timesteps'][i])
+        return raw, sketches, summaries, timesteps
+
+    def _summarizer_augment_dict(summarizer, dataset):
+        data, sketches, summaries, timesteps = _summarizer_augment(summarizer, dataset)
+        return {
+            "data": data,
+            # "sketch": sketches,
+            "summary": summaries,
+            "timesteps": timesteps
+        }
+
+    def _preprocessCods1(dialog: List[str]) -> str:
+        return "<s> <hl> {} <hl>".format(" <s> ".join(dialog[:]))
+
+    with_random_start_data = split_dataset.map(batched_random_start_augment,
+            batched=True, remove_columns=split_dataset.column_names, num_proc=4, batch_size=8)
+    cods1_preprocessed_data = with_random_start_data.map(lambda s: {"cods1": _preprocessCods1(s["rand_start"])}, num_proc=4)
+    summarizer = pipeline("summarization", model="Salesforce/cods-bart-large-xsum-samsum", device=0)
+    aug_data = cods1_preprocessed_data.map(lambda s: _summarizer_augment_dict(summarizer, s),
+            batched=True, remove_columns=cods1_preprocessed_data.column_names)
+    return aug_data
+
+def GetCods2(split_dataset):
+    '''
+    Cods2 augmentations are defined here.
+    Important note: we want to 1:many map each dialog to multiple dialogs, *while* remove existing columns.
+    Doing so requires a lot of care with respect to columns and only works at the train/validation/test split level.
+    The final size of this dataset should be # random timesteps * # dialogs.
+    '''
+
+    def _summarizer_augment(summarizer, dataset):
+        TLDR_TOKEN = "TLDR" # " TLDR "?
+        sketches = []
+        summaries = []
+        raw = []
+        timesteps = []
+        dialog_is = set()
+        for i, out in enumerate(summarizer(dataset['cods2_1'], batch_size=8)):
+            # Handle first sentence summary.
+            txt = out['summary_text']
+            if TLDR_TOKEN not in txt:
+                # We can't do anything so skip this sample.
+                continue
+            sketch, summary = txt.split(TLDR_TOKEN)
+            sketches.append([sketch])
+            summaries.append(summary)
+            raw.append(dataset['dialog'][i])
+            timesteps.append(dataset['timesteps'][i])
+            dialog_is.add(i)
+        
+        j = 0
+        for i, out in enumerate(summarizer(dataset['cods2_2'], batch_size=8)):        
+            # Handle second sentence summary.
+            txt = out['summary_text']
+            if i not in dialog_is or TLDR_TOKEN not in txt:
+                continue
+            sketch, summary = txt.split(TLDR_TOKEN)
+            sketches[j] = sketches[j].extend(sketch)
+            summaries[j] = "{} {}".format(summaries[j], summary)
+            j += 1
+        return raw, sketches, summaries, timesteps
+
+    def _summarizer_augment_dict(summarizer, dataset):
+        data, sketches, summaries, timesteps = _summarizer_augment(summarizer, dataset)
+        return {
+            "data": data,
+            "summary": summaries,
+            "timesteps": timesteps
+        }
+
+
+    def _preprocessCods2_1(dialog: List[str]) -> str:
+        h = int(len(dialog) / 2)
+        return "<s> <hl> {} <hl> {}".format(" <s> ".join(dialog[:h]), " <s> ".join(dialog[h:]))
+
+    def _preprocessCods2_2(dialog: List[str]) -> str:
+        h = int(len(dialog) / 2)
+        return "<s> {} <hl> {} <hl>".format(" <s> ".join(dialog[:h]), " <s> ".join(dialog[h:]))
+
+    with_random_start_data = split_dataset.map(batched_random_start_augment,
+            batched=True, remove_columns=split_dataset.column_names, num_proc=4, batch_size=8)
+    cods2_1_preprocessed_data = with_random_start_data.map(lambda s: {"cods2_1": _preprocessCods2_1(s["rand_start"])}, num_proc=4)
+    cods2_2_preprocessed_data = cods2_1_preprocessed_data.map(lambda s: {"cods2_2": _preprocessCods2_2(s["rand_start"])}, num_proc=4)
+
+    summarizer = pipeline("summarization", model="Salesforce/cods-bart-large-xsum-samsum", device=0)
+    aug_data = cods2_2_preprocessed_data.map(lambda s: _summarizer_augment_dict(summarizer, s),
+            batched=True, remove_columns=cods2_2_preprocessed_data.column_names)
+    return aug_data
 
 if __name__ == "__main__":
-    # Daily dialog Dataset loading & tokenization
-    # dataset = load_dataset("yelp_review_full")
+    CODS1_SAVE_PATH = "/home/derekhmd/summ_bot/data/DailySummaryCods1"
+    CODS2_SAVE_PATH = "/home/derekhmd/summ_bot/data/DailySummaryCods2"
+    CODS_END_SAVE_PATH = "/home/derekhmd/summ_bot/data/DailySummaryCodsEnd"
+
     dataset = load_dataset("daily_dialog")
     training_data = dataset['train']
     validation_data = dataset['validation']
     test_data = dataset['test']
-    import time
 
-    start_time = time.time()
-    LIMIT = 5000
+    subset = dataset
+    LIMIT = 20
     subset = dataset.filter(lambda e, i: i<LIMIT, with_indices=True)
     with_timesteps_data = subset.map(lambda s: {"start_times": GetTwoRandomTimesteps(s["dialog"])}, num_proc=4)
     # random_start_data = with_timesteps_data.map(random_start_augment, num_proc=4)
     
-    # In this example, we want to 1:many map each dialog to multiple dialogs, *while* remove existing columns.
-    # Doing so requires a lot of care with respect to columns and only works at the train/validation/test split level.
-    # The final size of this dataset should be # random timesteps * # dialogs.
-    training_with_starts_data = with_timesteps_data['train']
-    erandom_start_data = training_with_starts_data.map(batched_random_start_augment,
-        batched=True, remove_columns=training_with_starts_data.column_names, num_proc=4, batch_size=8)
-    cods1_preprocessed_data = erandom_start_data.map(lambda s: {"cods1": PreProcessCods1(s["rand_start"])}, num_proc=4)
-    summarizer = pipeline("summarization", model="Salesforce/cods-bart-large-xsum-samsum", device=0)
-    aug_data = cods1_preprocessed_data.map(lambda s: summarizer_augment_dict(summarizer, s),
-        batched=True, remove_columns=cods1_preprocessed_data.column_names)
-
+    # CODS 1 End Summaries.
+    start_time = time.time()
+    augmented_dataE = DatasetDict()
+    augmented_dataE['train'] = GetCodsEnd(with_timesteps_data['train'])
+    augmented_dataE['validation'] = GetCodsEnd(with_timesteps_data['validation'])
+    augmented_dataE['test'] = GetCodsEnd(with_timesteps_data['test'])
+    augmented_dataE.save_to_disk(CODS_END_SAVE_PATH)
     end_time = time.time()
     print(f"{end_time - start_time} time has passed!")
-    # need raw, timestep, and summary
+
+    # CODS 1 Summaries.
+    start_time = time.time()
+    augmented_data1 = DatasetDict()
+    augmented_data1['train'] = GetCods1(with_timesteps_data['train'])
+    augmented_data1['validation'] = GetCods1(with_timesteps_data['validation'])
+    augmented_data1['test'] = GetCods1(with_timesteps_data['test'])
+    augmented_data1.save_to_disk(CODS1_SAVE_PATH)
+    end_time = time.time()
+    print(f"{end_time - start_time} time has passed!")
+
+    # CODS 2 Summaries.
+    start_time = time.time()
+    augmented_data2 = DatasetDict()
+    augmented_data2['train'] = GetCods2(with_timesteps_data['train'])
+    augmented_data2['validation'] = GetCods2(with_timesteps_data['validation'])
+    augmented_data2['test'] = GetCods2(with_timesteps_data['test'])
+    augmented_data2.save_to_disk(CODS2_SAVE_PATH)
+    end_time = time.time()
+    print(f"{end_time - start_time} time has passed!")
+
+
+
+    for i in range (LIMIT):
+        print(f"Cods1 Summary: {augmented_data1['train']['summary'][i]}")
+        print(f"Cods2 Summary: {augmented_data2['train']['summary'][i]}")
+
+    # test = datasets.load_from_disk(SAVE_PATH)
+    
+    
